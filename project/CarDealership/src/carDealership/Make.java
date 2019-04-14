15c30823b5f6579e3deb7fb4d52026d9f04bc645
Frankie <Frankie@192.168.0.10>
2019-04-13 22:01:53 -0400

    added comments

--- project/CarDealership/src/carDealership/Make.java
+++ project/CarDealership/src/carDealership/Make.java
@@ -1,7 +1,14 @@
 package carDealership;
 
+/**
+ * A Class to hold the enumerations of the Car makes
+ */
 import java.util.*;
-
+/**
+ * @author Jose Miguelle Dela Paz <delapajo@sheridancollege.ca>
+ * @author Mark Tanglao <tanglao@sheridancollege.ca>
+ * @author Frankie Lau <laufra@sheridancollege.ca>
+ */
 public enum Make {
 	Toyota(0,"Toyota"),
 	Honda(1,"Honda"),
@@ -16,19 +23,28 @@ public enum Make {
 	Chevrolet(10,"Chevrolet"),
 	Volkswagen(11,"Volkswagen"),
 	Subaru(12,"Subaru");
-
+        
 	private String name;
         private int id;
+        /**
+         * Private constructor enumerations
+         * @param id
+         * @param name 
+         */
         private Make(int id,String name){
             this.id = id;
             this.name =name;
         }
-
+        
+        /**
+         * A method to return the name of the enumeration
+         * @return 
+         */
 	public String getName() {
 		return this.name;
 	}
 	/**
-	 * 
+	 * A method to set the name of the enumeration
 	 * @param name
 	 */
 	public void setName(String name) {
